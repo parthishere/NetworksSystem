a820ce4cf6728befee103fe0b54044165a20a8e6
parthishere <parthishere1234@gmail.com>
2025-01-30 13:53:55 -0700

    increased overall size to 2.5GB and added crc

--- uftp_client.c
+++ uftp_client.c
@@ -30,7 +30,7 @@
 #define TRANSMIT_SIZE 1024 * 30 // 30KB
 // this means that we can read uptp 5 mb in total as of now.
 #define DATA_SIZE 1024 * 1024 * 5 // 5MB buffer;
-#define HEADERSIZE 3
+#define HEADERSIZE 5
 
 #define END_OF_DYNAMIC_DATA "EOF\t\t\t\0"
 #define ACK "ack\t\t\t\0"
@@ -41,6 +41,34 @@
 
 #define clrscr ({printf("\033[2J\033[H");\
 fflush(stdout); })
+
+
+
+
+static unsigned char const crc8_table[] = {
+    0xea, 0xd4, 0x96, 0xa8, 0x12, 0x2c, 0x6e, 0x50, 0x7f, 0x41, 0x03, 0x3d,
+    0x87, 0xb9, 0xfb, 0xc5, 0xa5, 0x9b, 0xd9, 0xe7, 0x5d, 0x63, 0x21, 0x1f,
+    0x30, 0x0e, 0x4c, 0x72, 0xc8, 0xf6, 0xb4, 0x8a, 0x74, 0x4a, 0x08, 0x36,
+    0x8c, 0xb2, 0xf0, 0xce, 0xe1, 0xdf, 0x9d, 0xa3, 0x19, 0x27, 0x65, 0x5b,
+    0x3b, 0x05, 0x47, 0x79, 0xc3, 0xfd, 0xbf, 0x81, 0xae, 0x90, 0xd2, 0xec,
+    0x56, 0x68, 0x2a, 0x14, 0xb3, 0x8d, 0xcf, 0xf1, 0x4b, 0x75, 0x37, 0x09,
+    0x26, 0x18, 0x5a, 0x64, 0xde, 0xe0, 0xa2, 0x9c, 0xfc, 0xc2, 0x80, 0xbe,
+    0x04, 0x3a, 0x78, 0x46, 0x69, 0x57, 0x15, 0x2b, 0x91, 0xaf, 0xed, 0xd3,
+    0x2d, 0x13, 0x51, 0x6f, 0xd5, 0xeb, 0xa9, 0x97, 0xb8, 0x86, 0xc4, 0xfa,
+    0x40, 0x7e, 0x3c, 0x02, 0x62, 0x5c, 0x1e, 0x20, 0x9a, 0xa4, 0xe6, 0xd8,
+    0xf7, 0xc9, 0x8b, 0xb5, 0x0f, 0x31, 0x73, 0x4d, 0x58, 0x66, 0x24, 0x1a,
+    0xa0, 0x9e, 0xdc, 0xe2, 0xcd, 0xf3, 0xb1, 0x8f, 0x35, 0x0b, 0x49, 0x77,
+    0x17, 0x29, 0x6b, 0x55, 0xef, 0xd1, 0x93, 0xad, 0x82, 0xbc, 0xfe, 0xc0,
+    0x7a, 0x44, 0x06, 0x38, 0xc6, 0xf8, 0xba, 0x84, 0x3e, 0x00, 0x42, 0x7c,
+    0x53, 0x6d, 0x2f, 0x11, 0xab, 0x95, 0xd7, 0xe9, 0x89, 0xb7, 0xf5, 0xcb,
+    0x71, 0x4f, 0x0d, 0x33, 0x1c, 0x22, 0x60, 0x5e, 0xe4, 0xda, 0x98, 0xa6,
+    0x01, 0x3f, 0x7d, 0x43, 0xf9, 0xc7, 0x85, 0xbb, 0x94, 0xaa, 0xe8, 0xd6,
+    0x6c, 0x52, 0x10, 0x2e, 0x4e, 0x70, 0x32, 0x0c, 0xb6, 0x88, 0xca, 0xf4,
+    0xdb, 0xe5, 0xa7, 0x99, 0x23, 0x1d, 0x5f, 0x61, 0x9f, 0xa1, 0xe3, 0xdd,
+    0x67, 0x59, 0x1b, 0x25, 0x0a, 0x34, 0x76, 0x48, 0xf2, 0xcc, 0x8e, 0xb0,
+    0xd0, 0xee, 0xac, 0x92, 0x28, 0x16, 0x54, 0x6a, 0x45, 0x7b, 0x39, 0x07,
+    0xbd, 0x83, 0xc1, 0xff};
+
 
 typedef struct
 {
@@ -65,6 +93,23 @@ typedef enum
 
 commands_t print_menu();
 commands_t whichcmd(char *cmd);
+
+
+// Return the CRC-8 of data[0..len-1] applied to the seed crc. This permits the
+// calculation of a CRC a chunk at a time, using the previously returned value
+// for the next seed. If data is NULL, then return the initial seed. See the
+// test code for an example of the proper usage.
+unsigned crc8(unsigned crc, unsigned char const *data, size_t len)
+{
+    if (data == NULL)
+        return 0;
+    crc &= 0xff;
+    unsigned char const *end = data + len;
+    while (data < end)
+        crc = crc8_table[crc ^ *data++];
+    return crc;
+}
+
 
 void error(sockdetails_t *sd, char *msg)
 {
@@ -151,9 +196,18 @@ void list_files(sockdetails_t *sd)
             printf(GRN "\n\n-- Done -- \n\n" RESET);
             break;
         }
+        if (strncmp(recieve_buffer, ERROR_FOR_DYNAMIC_DATA, strlen(ERROR_FOR_DYNAMIC_DATA)) == 0)
+        {
+            printf(RED "[-] !!!!!!!~~~~~~~~~~ Error somewhere ~~~~~~~~~~!!!!!!! \n" RESET);
+            return;
+        }
+
+        int seq_num         = (((recieve_buffer[3] << 8) & 0xFF00) | (recieve_buffer[2] & 0x00FF));
+        int data_length     = (((recieve_buffer[1] << 8) & 0xFF00) | (recieve_buffer[0] & 0x00FF));
+        unsigned crc_server = recieve_buffer[4];
 
         char *temp_ip = getin_addr(sd->their_addr);
-        printf(MAG"> %s"RESET, &recieve_buffer[3]);
+        printf(MAG"> %s"RESET, &recieve_buffer[HEADERSIZE]);
 
         bzero(transmit_buffer, TRANSMIT_SIZE);
         if (recieve_buffer[2] == current_count)
@@ -182,6 +236,7 @@ void get_file(sockdetails_t *sd, char *filename)
     char whole_filename[60];
     snprintf(whole_filename, sizeof(whole_filename), "./Downloads/%s", filename);
     FILE *fp = fopen(whole_filename, "wb");
+    unsigned crc = crc8(0, NULL, 0);
     while (1)
     {
 
@@ -201,9 +256,11 @@ void get_file(sockdetails_t *sd, char *filename)
             return;
         }
 
-        int seq_num = (uint16_t)recieve_buffer[2];
+        int seq_num     = (((recieve_buffer[3] << 8) & 0xFF00) | (recieve_buffer[2] & 0x00FF));
         int data_length = (((recieve_buffer[1] << 8) & 0xFF00) | (recieve_buffer[0] & 0x00FF));
-
+        unsigned crc_server    = recieve_buffer[4];
+        crc = crc8(crc, &recieve_buffer[HEADERSIZE], data_length);
+        printf("CRC %d %d\n", crc_server, crc);
         bzero(transmit_buffer, TRANSMIT_SIZE);
 
         if (seq_num == current_count)
@@ -254,6 +311,7 @@ void put_file(sockdetails_t *sd)
     if(tcsetattr(STDIN_FILENO, TCSANOW, &new_tio)<0)perror("tcsetattr");
 
     int column_number = 1;
+    unsigned crc = crc8(0, NULL, 0);
     while (1)
     {
         char ch;
@@ -272,8 +330,6 @@ void put_file(sockdetails_t *sd)
                 printf("\033[1D");
                 printf(" ");
                 printf("\033[1D");
-                // column_number++;
-                // printf("\033[1C");
                 fflush(stdout);
                 continue;
             }
@@ -310,7 +366,11 @@ void put_file(sockdetails_t *sd)
             retry: ;
                 transmit_buffer[0] = ((write_pointer - HEADERSIZE) & 0xFF);
                 transmit_buffer[1] = (((write_pointer - HEADERSIZE) & 0xFF00) >> 8);
-                transmit_buffer[2] = seq_number;
+                transmit_buffer[2] = (seq_number & 0x00FF);
+                transmit_buffer[3] = (seq_number & 0xFF00) >> 8;
+                crc8(crc, &transmit_buffer[HEADERSIZE], TRANSMIT_SIZE - HEADERSIZE); // crc
+                printf("CRC %d\n", crc);
+                transmit_buffer[4] = crc;
 
                 _send(sd, TRANSMIT_SIZE, transmit_buffer);
                 write_pointer = HEADERSIZE;
@@ -322,6 +382,7 @@ void put_file(sockdetails_t *sd)
 
                     bzero(transmit_buffer, TRANSMIT_SIZE);
                     seq_number++;
+                    if(seq_number >= 0xFFFF) break;
                 }
                 else
                 {
@@ -352,91 +413,6 @@ void put_file(sockdetails_t *sd)
     tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
 }
 
-// void put_file(sockdetails_t *sd)
-// {
-//     char recieve_buffer[RECIEVE_SIZE];
-//     char transmit_buffer[TRANSMIT_SIZE];
-//     int write_pointer = HEADERSIZE; // 0, 1, 2 are filled
-//     int seq_number = 0;
-//     int retry_count = 0;
-//     bool send_data = false, end_of_data = false;
-//     // char *write_pointer = transmit_buffer;
-//     // reddit artical about changing behaviour of the unix
-//     struct termios old_tio, new_tio;
-//     unsigned char c;
-//     // Get current terminal settings
-//     tcgetattr(STDIN_FILENO, &old_tio);
-//     new_tio = old_tio;
-//     // Disable canonical mode and echo
-//     new_tio.c_lflag &= (~ICANON & ~ECHO);
-//     tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
-//     while (1)
-//     {
-//         char ch;
-//         read(STDIN_FILENO, &ch, 1);
-//         // char ch = getc(stdin); // Get character without echo
-//         printf(MAG"entered char %c\n" RESET, ch);
-//         if (write_pointer >= 10 || ch == 27) // -1 as it is index, and index starts from 0;
-//         // if (write_pointer >= TRANSMIT_SIZE - 1 - HEADERSIZE) // -1 as it is index, and index starts from 0;
-//         {
-//             if (ch == 27 && write_pointer > 0)
-//             {
-//                 send_data = true;
-//                 end_of_data = true;
-//             }
-//             else if (ch != 27 && write_pointer > 0)
-//             {
-//                 send_data = true;
-//                 end_of_data = false;
-//             }
-//             else if (ch == 27 && write_pointer == 0)
-//             {
-//                 send_data = false;
-//                 end_of_data = true;
-//             }
-//             if (send_data)
-//             {
-//             retry: ;
-//                 transmit_buffer[0] = ((write_pointer - HEADERSIZE) & 0xFF);
-//                 transmit_buffer[1] = (((write_pointer - HEADERSIZE) & 0xFF00) >> 8);
-//                 transmit_buffer[2] = seq_number;
-//                 _send(sd, TRANSMIT_SIZE, transmit_buffer);
-//                 write_pointer = HEADERSIZE;
-//                 bzero(recieve_buffer, TRANSMIT_SIZE);
-//                 _recv(sd, RECIEVE_SIZE, recieve_buffer);
-//                 if (strncmp(recieve_buffer, ACK, 7) == 0)
-//                 {
-//                     bzero(transmit_buffer, TRANSMIT_SIZE);
-//                     seq_number++;
-//                 }
-//                 else
-//                 {
-//                     retry_count++;
-//                     if (retry_count >= 3)
-//                     {
-//                         retry_count = 0;
-//                         printf("Max retries reached. Aborting.\n");
-//                         break;
-//                     }
-//                     printf("retry \n\r");
-//                     goto retry;
-//                 }
-//             }
-//             if (end_of_data)
-//             {
-//                 seq_number = 0;
-//                 printf("End of file sent !\n\n\n");
-//                 _send(sd, strlen(END_OF_DYNAMIC_DATA), END_OF_DYNAMIC_DATA);
-//                 end_of_data = false;
-//                 break;
-//             }
-//         }
-//         transmit_buffer[write_pointer++] = ch;
-//     }
-//     tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
-// }
-
-
 void delete_file(sockdetails_t *sd, char* filename){
     printf("\n\nDELETE\n\n");
 
@@ -458,6 +434,15 @@ void delete_file(sockdetails_t *sd, char* filename){
 
 void cleanup_resources(sockdetails_t *sd){
 
+    char recieve_buffer[RECIEVE_SIZE];
+
+    bzero(recieve_buffer, TRANSMIT_SIZE);
+    _recv(sd, RECIEVE_SIZE, recieve_buffer);
+    if(strncmp(recieve_buffer, ACK, strlen(ACK))){
+        // done
+        close(sd->sockfd);
+        exit(EXIT_SUCCESS);
+    }
 }
 
 int main(int argc, char *argv[])
